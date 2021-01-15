63639ef9a96367ee7d951de40d6a9e4ace32b0fc
joseph-njogu <josephnjogu487@gmail.com>
2021-01-15 23:22:20 +0300

    Oauth2 inialize
    Initialized oauth2 using google.

---
+++ Cryptotrade/bitcotrade/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('api/login', views.login, name='login'),
+    path('', TemplateView.as_view(template_name ='index.html')),
+    path('', views.dashboard, name='dashboard'),
+    path('home', views.homepage, name='homepage'),
+]
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
